088e7e99a2effa7b949741566c9e3222d6702745
matt maddex <mmaddex@render.com>
2023-06-13 12:48:53 -0600

    Update updateConfig.js

--- updateConfig.js
+++ updateConfig.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const contents = fs.readFileSync("config.production.json");
 const config = JSON.parse(contents);
 const externalURL = process.env.url || process.env.RENDER_EXTERNAL_URL;
+console.log(externalURL)
 if (externalURL) {
   // update the URL in the config
   config.url = externalURL;

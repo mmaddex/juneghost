151439ac14b045a1bc6c276eba29c429fe5aa07b
matt maddex <mmaddex@render.com>
2023-06-13 12:54:14 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,14 @@
 set -o errexit
 
 echo $GHOST_INSTALL
-echo $baseDir
+echo $GHOST_CONTENT
 baseDir="$GHOST_INSTALL/content.orig"
 	for src in "$baseDir"/*/ "$baseDir"/themes/*; do
 		src="${src%/}"
 		target="$GHOST_CONTENT/${src#$baseDir/}"
 		mkdir -p "$(dirname "$target")"
 		if [ ! -e "$target" ]; then
+		        echo "HERE HEAR"
 			tar -cC "$(dirname "$src")" "$(basename "$src")" | tar -xC "$(dirname "$target")"
 		fi
 	done
